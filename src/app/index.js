848bf931fecfee99ffec0771a2fb169fcefb60a6
Gabriel Fajardo @ Gabs <gabo2023brazil@gmail.com>
2025-06-03 18:12:47 -0500

    Defining right onmessage method

--- src/app/index.js
+++ src/app/index.js
@@ -4,30 +4,46 @@ import { filterByDate } from '../data/queries.js';
 import { calculatePastDate } from '../utils/calculateDate.js';
 
 sensorTypes.forEach(sensorType => {
-  const topic = `app/${sensorType}/query`;
+  const topic = `backend/${sensorType}/req`;
 
   if (!collections[sensorType]) {
     console.warn(`[⚠️ WARN] No schema found for sensor type: ${sensorType}`);
     return;
   }
 
-  client.subscribe(topic, async (error, message) => {
-    if (!error) {
-      try {
+  client.subscribe(topic)
+  .then(() => {
+    console.log(`[📬 SUBSCRIBED] Subscribed to topic: ${topic}`);
+  })
+  .catch(err => {
+    console.error(`[❌ ERROR] Failed to subscribe to topic ${topic}: ${err}`);
+  });
+})
+
+client.on("message", async (topic, message) => {
+  console.log(`[📬 MESSAGE] Received message on topic ${topic}: ${message.toString()}`);
+  try {
         const date = calculatePastDate(message.toString());
+        console.log(`[📅 DATE] Calculated date: ${date}`);
   
         if (!date) {
           console.error('[❌ ERROR] Date missing or invalid')
           return;
         }
+        const schema = topic.charAt(0).toUpperCase() +topic.slice(1);
         
-        const data = await filterByDate(date, collections[sensorType]);
-        publishToTopic(`app/${sensorType}/response`, JSON.stringify(data));
+        const data = await filterByDate(date, schema);
+        const topicToPublish = topic.replace('/req', '/res');
+        
+        setTimeout(() => {
+          publishToTopic(topicToPublish, JSON.stringify(data));
+        }, 1300);
+        
+        
+
       } catch (error) {
         console.error(`[❌ ERROR] Error processing message: ${error}`);
       }
-    } else {
-      console.error(`[⛔ ERROR] Error subscribing to topic ${topic}: ${error}`);
-    }
-  });
+  
+  
 });
--- src/data/queries.js
+++ src/data/queries.js
@@ -1,5 +1,8 @@
 export const filterByDate = (date, schema) => {
   const start = new Date(date);
+
+  console.log("Filtering data from:", start);
+  // Assuming the end date is the current date
   const end = new Date();
   
   return schema.find({
